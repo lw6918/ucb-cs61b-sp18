0e877aeaa626a53258cc3825b780780fcb2f5be5
lw6918 <2917820515@qq.com>
2022-05-21 15:20:24 +0800

    bugfix

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -6,21 +6,23 @@ import org.junit.Test;
 public class Percolation {
     int[][] arr;
     WeightedQuickUnionUF weightedQuickUnionUF = null;
-    int virtualTopSite = -1;
-    int virtualBottomSite = -2;
+    int virtualTopSite;
+    int virtualBottomSite;
     int openSize = 0;
     public Percolation(int N) {
         // create N-by-N grid, with all sites initially blocked
+        arr = new int[N][N];
+        virtualTopSite = N * N;
+        virtualBottomSite = N * N + 1;
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 arr[i][j] = 0;
             }
         }
-        weightedQuickUnionUF = new WeightedQuickUnionUF(N * N + 1);
-        for (int i = 0; i < N; i++) {
-            weightedQuickUnionUF.union(virtualTopSite, xyTo1D(0, i));
-            weightedQuickUnionUF.union(virtualBottomSite, xyTo1D(N - 1, i));
-        }
+        weightedQuickUnionUF = new WeightedQuickUnionUF(N * N + 2);
     }
 
     private int xyTo1D(int r, int c) {
@@ -42,14 +44,22 @@ public class Percolation {
         if (arr[row][col] == 0) {
             arr[row][col] = 1;
             openSize += 1;
+            if (row == 0) {
+                weightedQuickUnionUF.union(virtualTopSite, xyTo1D(row, col));
+            } else if (row == arr[0].length - 1) {
+                weightedQuickUnionUF.union(virtualBottomSite, xyTo1D(row, col));
+            }
             if (!(indexOutOfBounds(row - 1, col)) && isOpen(row - 1, col)) {
                 weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(row - 1, col));
-            } else if (!(indexOutOfBounds(row + 1, col)) && isOpen(row + 1, col)) {
+            }
+            if (!(indexOutOfBounds(row + 1, col)) && isOpen(row + 1, col)) {
                 weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(row + 1, col));
-            } else if (!(indexOutOfBounds(row, col - 1)) && isOpen(row, col - 1)) {
+            }
+            if (!(indexOutOfBounds(row, col - 1)) && isOpen(row, col - 1)) {
                 weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(row, col - 1));
-            } else if (!(indexOutOfBounds(row, col + 1)) && isOpen(row, col + 1)) {
-                weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+            }
+            if (!(indexOutOfBounds(row, col + 1)) && isOpen(row, col + 1)) {
+                weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(row, col + 1));
             }
         }
 
