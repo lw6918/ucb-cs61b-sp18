665866fb1451719672aa77c67477d0551fbca2b8
lw6918 <2917820515@qq.com>
2022-05-21 14:38:41 +0800

    done with hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,77 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import org.junit.Test;
 
 public class Percolation {
+    int[][] arr;
+    WeightedQuickUnionUF weightedQuickUnionUF = null;
+    int virtualTopSite = -1;
+    int virtualBottomSite = -2;
+    int openSize = 0;
+    public Percolation(int N) {
+        // create N-by-N grid, with all sites initially blocked
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = 0;
+            }
+        }
+        weightedQuickUnionUF = new WeightedQuickUnionUF(N * N + 1);
+        for (int i = 0; i < N; i++) {
+            weightedQuickUnionUF.union(virtualTopSite, xyTo1D(0, i));
+            weightedQuickUnionUF.union(virtualBottomSite, xyTo1D(N - 1, i));
+        }
+    }
 
+    private int xyTo1D(int r, int c) {
+        return r * arr[0].length + c;
+    }
+
+    private boolean indexOutOfBounds(int row, int col) {
+        if (!(row >= 0 && row < arr.length && col >= 0 && col < arr.length)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void open(int row, int col) {
+        // open the site (row, col) if it is not open already
+        if (indexOutOfBounds(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (arr[row][col] == 0) {
+            arr[row][col] = 1;
+            openSize += 1;
+            if (!(indexOutOfBounds(row - 1, col)) && isOpen(row - 1, col)) {
+                weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+            } else if (!(indexOutOfBounds(row + 1, col)) && isOpen(row + 1, col)) {
+                weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+            } else if (!(indexOutOfBounds(row, col - 1)) && isOpen(row, col - 1)) {
+                weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+            } else if (!(indexOutOfBounds(row, col + 1)) && isOpen(row, col + 1)) {
+                weightedQuickUnionUF.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+            }
+        }
+
+    }
+    public boolean isOpen(int row, int col) {
+        // is the site (row, col) open?
+        if(arr[row][col] == 1) {
+            return true;
+        }
+        return false;
+    }
+    public boolean isFull(int row, int col) {
+        // is the site (row, col) full?
+        return weightedQuickUnionUF.connected(xyTo1D(row, col), virtualTopSite);
+    }
+    public int numberOfOpenSites() {
+        // number of open sites
+        return openSize;
+    }
+    public boolean percolates() {
+        // does the system percolate?
+        return weightedQuickUnionUF.connected(virtualTopSite, virtualBottomSite);
+    }
+//    public static void main(String[] args)   // use for unit testing (not required)
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,35 @@
 package hw2;
-
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 public class PercolationStats {
-
+    Percolation p = null;
+    int[] data;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        // perform T independent experiments on an N-by-N grid
+        data = new int[T];
+        for (int t = 0; t < T; t++) {
+            p = pf.make(N);
+            while(!p.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                p.open(row, col);
+            }
+            data[t] = p.numberOfOpenSites();
+        }
+    }
+    public double mean() {
+        // sample mean of percolation threshold
+        return StdStats.mean(data);
+    }
+    public double stddev() {
+        // sample standard deviation of percolation threshold
+        return StdStats.stddev(data);
+    }
+    public double confidenceLow() {
+        // low endpoint of 95% confidence interval
+        return mean() - 1.96 * stddev() / Math.sqrt(data.length);
+    }
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(data.length);
+    }
 }
